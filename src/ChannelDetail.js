cb351a2975e16bb464f749772c9f64f3d2b1ed14
NazarulxFitri <63062783+NazarulxFitri@users.noreply.github.com>
2021-09-20 22:32:25 +0800

    final commit

--- src/ChannelDetail.js
+++ src/ChannelDetail.js
@@ -4,31 +4,28 @@ import './ChannelDetail.css';
 const ChannelDetail = (props) => {
 
     const [activeChannel, setActiveChannel] = useState([]);
-    const [activeSchedule, setActiveSchedules] = useState([]);
     const [scheduleDate, setScheduleDate] = useState([]);
     const [scheduleList, setScheduleList] = useState([]);
     const [activeTab, setActiveTab] = useState(0);
 
-    useEffect(async () => {
+    const fetchData = async () => {
         const channel_id = props.location.state.channel_id;
         const api_call = await fetch(`https://contenthub-api.eco.astro.com.my/channel/${channel_id}.json`);
         const api_res = await api_call.json();
         const Date = Object.keys(api_res.response.schedule);
         const List = Object.values(api_res.response.schedule);
-        //console.log(api_res.response.schedule);
-        //this.setState({ activeChannel: api_res.response, activeSchedules: api_res.response.schedule });
         setActiveChannel(api_res.response);
-        setActiveSchedules(api_res.response.schedule);
         setScheduleDate(Date);
         setScheduleList(List);
-    }, []);
+    }
+
+    useEffect(() => {
+      fetchData()  
+    });
 
     console.log(scheduleList);
 
     const channel = activeChannel;
-    const schedules = activeSchedule;
-
-    //console.log(schedules);
 
     return (
 
@@ -45,7 +42,6 @@ const ChannelDetail = (props) => {
                 {
                     scheduleDate.map((keyName, index) => {
                         let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                        const timeStamp = Date.parse(keyName);
                         let dateObject = new Date(keyName);
                         const days = daysOfWeek[dateObject.getDay()];
 
@@ -53,7 +49,7 @@ const ChannelDetail = (props) => {
                             <div key={keyName + index}>
                                 <div
                                     className="channel_date"
-                                    style={{ color: activeTab == index ? "red" : "blue" }}
+                                    style={{ color: activeTab === index ? "red" : "blue" }}
                                     onClick={() => setActiveTab(index)}
                                 >{days}</div>
                             </div>
