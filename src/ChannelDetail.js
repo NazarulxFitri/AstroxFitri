191d87353e008bf750e73eda7e64ab89a00f7909
NazarulxFitri <63062783+NazarulxFitri@users.noreply.github.com>
2021-09-20 22:21:35 +0800

    fix bugs , UI mobile view

--- src/ChannelDetail.js
+++ src/ChannelDetail.js
@@ -1,47 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ChannelDetail.css';
 
-class ChannelDetail extends React.Component {
-    state = {
-        activeChannel: [],
-        activeSchedules: []
-    }
+const ChannelDetail = (props) => {
 
-    componentDidMount = async () => {
-        const channel_id = this.props.location.state.channel_id;
+    const [activeChannel, setActiveChannel] = useState([]);
+    const [activeSchedule, setActiveSchedules] = useState([]);
+    const [scheduleDate, setScheduleDate] = useState([]);
+    const [scheduleList, setScheduleList] = useState([]);
+    const [activeTab, setActiveTab] = useState(0);
+
+    useEffect(async () => {
+        const channel_id = props.location.state.channel_id;
         const api_call = await fetch(`https://contenthub-api.eco.astro.com.my/channel/${channel_id}.json`);
         const api_res = await api_call.json();
-        this.setState({ activeChannel: api_res.response, activeSchedules: api_res.response.schedule });
-    }
+        const Date = Object.keys(api_res.response.schedule);
+        const List = Object.values(api_res.response.schedule);
+        //console.log(api_res.response.schedule);
+        //this.setState({ activeChannel: api_res.response, activeSchedules: api_res.response.schedule });
+        setActiveChannel(api_res.response);
+        setActiveSchedules(api_res.response.schedule);
+        setScheduleDate(Date);
+        setScheduleList(List);
+    }, []);
 
+    console.log(scheduleList);
 
-    render() {
-        const channel = this.state.activeChannel;
-        const schedules = this.state.activeSchedules;
+    const channel = activeChannel;
+    const schedules = activeSchedule;
 
-        console.log(schedules);
+    //console.log(schedules);
 
-        return (
-            <div className="channel">
-                <div className="channel__header">
-                    <img className="channel__img" src={channel['imageUrl']} alt={channel["title"]} />
-                    <div className="channel__rightSection">
-                        <p className="channelCard__number">{channel["stbNumber"]}</p>
-                        <p className="channelCard__title">{channel["title"]}</p>
-                    </div>
-                </div>
-                <p className="channel__description">{channel["description"]}</p>
-                <div className="channel__schedule">
-                    {
-                        Object.keys(schedules).map((keyName, index) => (
+    return (
 
-                            <div key={keyName + index}>{keyName}</div>
-                        ))
-                    }
+        <div className="channel">
+            <div className="channel__header">
+                <img className="channel__img" src={channel['imageUrl']} alt={channel["title"]} />
+                <div className="channel__rightSection">
+                    <p className="channelCard__number">{channel["stbNumber"]}</p>
+                    <p className="channelCard__title">{channel["title"]}</p>
                 </div>
             </div>
-        );
-    }
+            <p className="channel__description">{channel["description"]}</p>
+            <div className="channel_detail">
+                {
+                    scheduleDate.map((keyName, index) => {
+                        let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                        const timeStamp = Date.parse(keyName);
+                        let dateObject = new Date(keyName);
+                        const days = daysOfWeek[dateObject.getDay()];
+
+                        return (
+                            <div key={keyName + index}>
+                                <div
+                                    className="channel_date"
+                                    style={{ color: activeTab == index ? "red" : "blue" }}
+                                    onClick={() => setActiveTab(index)}
+                                >{days}</div>
+                            </div>
+                        )
+
+                    })
+                }
+
+            </div>
+            <div>{scheduleList[activeTab]?.map((object, i) => {
+                console.log("i k", object);
+                return (
+                    <div className="channel_scheduleList" key={object.title + i}> 
+                        <div className="channel_time">{object.datetime}</div>
+                        <div>{object.title}</div>
+                        
+                    </div>  
+                )
+            })}</div>
+        </div>
+    );
+
 }
 
 export default ChannelDetail;
